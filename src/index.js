f71d28961df65dbcab127862142e07ea5ce8e069
IotyRandri <randrianarivoioty@gmail.com>
2025-06-08 23:17:14 +0300

    Moved Navbar to top for UI/UX

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
+    {/* Add navbar component for shared layout */}
+    <Navbar />
 
     {/* Main Component / Home Page */}
     <App />
     
-    {/* Add navbar component for shared layout */}
-    <Navbar />
     
   </React.StrictMode>
 );
